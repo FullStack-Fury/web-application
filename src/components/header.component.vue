f61e363100085b3ca75657ffba33bb125211f5b2
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-09-20 23:49:18 -0500

    feat: added menuBar

---
+++ src/components/header.component.vue
@@ -0,0 +1,52 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "headerContent",
+  data(){
+    return {
+      items: [
+        {
+          label: "Inicio",
+          icon: "pi pi-home"
+        },
+        {
+          label: "Productos",
+          icon: "pi pi-tags"
+        },
+        {
+          label: "Proceso",
+          icon: "pi pi-cog"
+        },
+        {
+          label: "Más opciones",
+          icon: "pi pi-ellipsis-v"
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <pv-menubar :model="items" class="menuBar"/>
+  </div>
+</template>
+
+<style scoped>
+
+.card {
+  width: 100%;
+  padding: 0px;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.menuBar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
