70d82f527515192b9483f3ce82d222225dfc9aae
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-09-20 23:48:47 -0500

    chore: import and configure the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Avatar from "primevue/avatar";
+import Menubar from "primevue/menubar";
+import PrimeVue from 'primevue/config';
+import Badge from "primevue/badge";
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    ripple: true, theme: {
+        preset: Aura, options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.component('pv-avatar', Avatar)
+    .component('pv-menubar', Menubar)
+    .component('pv-badge', Badge)
+
+app.mount('#app')
