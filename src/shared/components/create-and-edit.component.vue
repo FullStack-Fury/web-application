4cf8276db0f72dc1a3ec8c31a71065271a02a6a2
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-24 13:27:34 -0500

    feat(shared): create reusable components.

---
+++ src/shared/components/create-and-edit.component.vue
@@ -0,0 +1,36 @@
+<script>
+const defaultStyle = { width: '400px'};
+
+export default {
+  name: "create-and-edit",
+  props: { entity: null, visible: Boolean, entityName: '', edit: Boolean, size: 'default'},
+  methods: {
+    onCancel() {
+      this.$emit('canceled');
+    },
+    onSave() {
+      this.$emit('saved', this.entity);
+    },
+    getHeaderTitle() {
+      return `${this.edit ? 'Edit' : 'New'} ${this.entityName}`;
+    },
+    getSubmitLabel() {
+      return this.edit ? 'Update' : 'Create';
+    },
+    getDialogStyle() {
+      let dialogStyle = defaultStyle;
+      dialogStyle = this.size === 'standard' ? { width: '600px' } : defaultStyle;
+      dialogStyle = this.size === 'large' ? { width: '900px' } : dialogStyle;
+      return dialogStyle;
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
