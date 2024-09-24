1229bd79010ece1670c06e5a52ac095b10d616ac
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-24 16:26:08 -0500

    feat(route): setup routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import {createRouter, createWebHistory} from "vue-router";
-
+import MaterialManagementComponent from "../dashboard/pages/material-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
-
+    routes: [
+        { path: '/dashboard/materials',   name: 'materials', component: MaterialManagementComponent, meta: { title: 'Inventory Management'}},
+        { path: '/',                        redirect: '/dashboard/materials'}
+    ]
 });
 
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'GemZ Dashboard';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    next();
+})
 export default router;
