c4e89e60c6bf1ec37b28163312b27e27f5e1c33d
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-25 12:43:13 -0500

    feat(service): added employee service.

---
+++ src/inventory/services/employee.service.js
@@ -0,0 +1,25 @@
+import http from "../../shared/services/http-common.js";
+
+export class EmployeeService {
+    resourceEndpoint = '/employees';
+
+    getAll() {
+        return http.get(this.resourceEndpoint);
+    }
+
+    getById(id) {
+        return http.get(`${this.resourceEndpoint}/${id}`);
+    }
+
+    create(employeeResource) {
+        return http.post(this.resourceEndpoint, employeeResource);
+    }
+
+    update(id, employeeResource) {
+        return http.put(`${this.resourceEndpoint}/${id}`, employeeResource);
+    }
+
+    delete(id) {
+        return http.delete(`${this.resourceEndpoint}/${id}`);
+    }
+}
