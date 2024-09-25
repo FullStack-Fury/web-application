a137b7585bfa419c3607ea9e41dc6f36a5679f27
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-25 02:57:43 -0500

    feat(route): added route.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
 import MaterialManagementComponent from "../inventory/pages/material-management.component.vue";
+import productManagement from "../inventory/pages/product-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/management', name: 'management', component: productManagement, meta: { title: 'Product Management'}},
         { path: '/inventory/materials',   name: 'materials', component: MaterialManagementComponent, meta: { title: 'Inventory Management'}},
         { path: '/',                        redirect: '/inventory/materials'}
     ]
