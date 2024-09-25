c3d9e7c2504cd569f9ee64777da2d57648e1131d
AngeloC12 <angelocuri@hotmail.com>
2024-09-25 15:28:15 -0500

    feat: Added status attribute to product

--- src/inventory/model/product.entity.js
+++ src/inventory/model/product.entity.js
@@ -1,8 +1,9 @@
 export class Product {
-    constructor({ id = '', name = '', materials = [], employeeId = null } = {}) {
+    constructor({ id = '', name = '', materials = [], employeeId = null, status = '' } = {}) {
         this.id = id;
         this.name = name;
         this.materials = materials; // Array de objetos { materialId, quantity }
         this.employeeId = employeeId;
+        this.status = status;
     }
 }
