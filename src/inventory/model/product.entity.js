2113e43889ac55f77c4825cb37359c6b0c2b1d2b
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-25 12:43:38 -0500

    feat(entity): added employee entity to product.

--- src/inventory/model/product.entity.js
+++ src/inventory/model/product.entity.js
@@ -1,7 +1,8 @@
 export class Product {
-    constructor({ id = null, name = '', materials = [] } = {}) {
+    constructor({ id = '', name = '', materials = [], employeeId = null } = {}) {
         this.id = id;
         this.name = name;
-        this.materials = materials;
+        this.materials = materials; // Array de objetos { materialId, quantity }
+        this.employeeId = employeeId;
     }
 }
